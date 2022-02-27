e8ebf853600397682431599f1a5da4d4e100c2ef
Nedim AKAR <nedim.akar53411@gmail.com>
2022-02-27 23:39:21 +0100

    docs: add comments to req-res/api-gateway

--- request-response/api-gateway/main.py
+++ request-response/api-gateway/main.py
@@ -7,18 +7,27 @@ PORT = os.getenv('PORT', 8080)
 app = Flask(__name__)
 
 @app.route('/api')
-def hello_world():
+
+def apiGateway():
+    # Parse username query
     username = flask.request.args.get("username")
+    
+    # Fetching connections from connections service with username query 
     r = requests.get(url = "http://localhost:8081/connections?username=" + username)
     data = r.json()
 
+    # Creating In-memory store
     favoritePosts = {} 
     
+    # Getting posts by usernames which usernames I take by connections service as []string
     for item in data :
         r = requests.get(url = "http://localhost:8082/posts?username=" + item)
         data = r.json()
+        
+        # Appending to our In-memory store
         favoritePosts[ item ] = data
     
+    # Returning In-memory store
     return str(favoritePosts)
 
 app.run(host='0.0.0.0', port=PORT)
