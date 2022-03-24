6e1d527696609e323736bffb95a8e25c4dc123a4
Nedim Akar <nedim.akar53411@gmail.com>
2022-03-24 17:57:51 +0300

    chore: optimize logger

--- event-driven/connection-service/configs/configs.js
+++ event-driven/connection-service/configs/configs.js
@@ -1,12 +1,14 @@
 import winston from "winston";
 import CONSTANTS from "../constants/constants.js";
 
+const format = winston.format.combine(
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss:ms" }),
+    winston.format.colorize({ all: true }),
+    winston.format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
+);
+
 export const logger = winston.createLogger({
-    format: winston.format.json(),
+    format: format,
     defaultMeta: { service: CONSTANTS.SERVICE_NAME },
-    transports: [
-        new winston.transports.Console(),
-        new winston.transports.File({ filename: "error.log", level: "error" }),
-        new winston.transports.File({ filename: "combined.log" }),
-    ],
+    transports: [new winston.transports.Console()],
 });
